93ba0b71b8aa82e3e784baca0a2c55405e8a7978
Maxiomm <maxiom75@gmail.com>
2025-03-18 15:48:28 +0100

    Setup project

---
+++ src/pages/HomePage.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+export default function HomePage() {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f0f0f0",
+    padding: 20,
+  },
+});
